34a6f47bd47b72be49a0b991fcc6801db0c7d566
Eugene Zagidullin <e.asphyx@gmail.com>
2022-10-19 00:32:09 +0300

    doc WIP

--- ed25519/ed25519.go
+++ ed25519/ed25519.go
@@ -1,3 +1,7 @@
+/*
+Package ed25519 deals with SLIP-10 Ed25519 keys.
+Unlike ECDSA SLIP-10 keys these keys can be used for hardened derivation only.
+*/
 package ed25519
 
 import (
@@ -15,15 +19,20 @@ import (
 const ed25519Key = "ed25519 seed"
 
 const (
+	// MinSeedSize is the minimal allowed seed byte length
 	MinSeedSize = 16
+	// MaxSeedSize is the maximal allowed seed byte length
 	MaxSeedSize = 64
 )
 
 var (
+	// ErrNonHardened is returned when an attempt to use non hardened path was made
 	ErrNonHardened = errors.New("ed25519: non hardened derivation")
-	ErrPublic      = errors.New("ed25519: can't use public key for derivation")
+	// ErrPublic is returned when an attempt to derive a public key's child was made
+	ErrPublic = errors.New("ed25519: can't use public key for derivation")
 )
 
+// PrivateKey is the extended Ed25519 private key. It implements hdw.PrivateKey
 type PrivateKey struct {
 	ed25519.PrivateKey
 	ChainCode []byte
@@ -35,6 +44,7 @@ func hmacSha512(data []byte, key []byte) []byte {
 	return hmac.Sum(nil)
 }
 
+// Derive returns a child key of the receiver using a single index
 func (s *PrivateKey) Derive(index uint32) (hdw.PrivateKey, error) {
 	data := make([]byte, 37)
 	if index&hdw.Hard == 0 {
@@ -49,6 +59,7 @@ func (s *PrivateKey) Derive(index uint32) (hdw.PrivateKey, error) {
 	}, nil
 }
 
+// Derive returns a child key of the receiver using a full path
 func (s *PrivateKey) DerivePath(path hdw.Path) (hdw.PrivateKey, error) {
 	if k, err := hdw.Derive(s, path); err != nil {
 		return nil, err
@@ -57,10 +68,12 @@ func (s *PrivateKey) DerivePath(path hdw.Path) (hdw.PrivateKey, error) {
 	}
 }
 
+// Chain returns the chain code
 func (p *PrivateKey) Chain() []byte {
 	return p.ChainCode
 }
 
+// Public returns the extended public key corresponding to the receiver
 func (p *PrivateKey) ExtendedPublic() hdw.PublicKey {
 	return &PublicKey{
 		PublicKey: p.Public().(ed25519.PublicKey),
@@ -68,31 +81,38 @@ func (p *PrivateKey) ExtendedPublic() hdw.PublicKey {
 	}
 }
 
+// Naked returns the naked private key that can be used with the standard Go crypto library
 func (p *PrivateKey) Naked() crypto.PrivateKey {
 	return p.PrivateKey
 }
 
+// PublicKey is the extended Ed25519 public key. It implements hdw.PublicKey
 type PublicKey struct {
 	ed25519.PublicKey
 	ChainCode []byte
 }
 
+// Chain returns the chain code
 func (p *PublicKey) Chain() []byte {
 	return p.ChainCode
 }
 
+// Naked returns the naked public key that can be used with the standard Go crypto library
 func (p *PublicKey) Naked() crypto.PublicKey {
 	return p.PublicKey
 }
 
+// As SLIP10-Ed25519 doesn't support non hardened derivation this function always returns ErrPublic
 func (p *PublicKey) Derive(index uint32) (hdw.PublicKey, error) {
 	return nil, ErrPublic
 }
 
+// As SLIP10-Ed25519 doesn't support non hardened derivation this function always returns ErrPublic
 func (s *PublicKey) DerivePath(path hdw.Path) (hdw.PublicKey, error) {
 	return nil, ErrPublic
 }
 
+// NewKeyFromSeed generates the root keys from the seed as specified in SLIP-10
 func NewKeyFromSeed(seed []byte) *PrivateKey {
 	if len(seed) < MinSeedSize || len(seed) > MaxSeedSize {
 		panic(fmt.Sprintf("bad seed size %d", len(seed)))
--- hdw.go
+++ hdw.go
@@ -1,3 +1,7 @@
+/*
+Package hdw provides a unified API for dealing with hierarchical deterministic keys
+also known as hierarchical deterministic wallets in application to blockchains as defined in BIP-32 and SLIP-10
+*/
 package hdw
 
 import (
@@ -5,45 +9,64 @@ import (
 	"crypto/sha512"
 	"errors"
 	"fmt"
-	"io"
 	"strconv"
 	"strings"
 
 	"golang.org/x/crypto/pbkdf2"
 )
 
-const (
-	ChainCodeSize        = 32
-	Hard          uint32 = 1 << 31
-)
+// ChainCodeSize is the length of chain code common for all key types
+const ChainCodeSize = 32
 
+// Hard is the flag signifying so called hardened derivation if added to the derivation index.
+// The exact use of it is specific to the chosen algorithm but in general it means that the private key is used
+// to build the hash chain
+const Hard uint32 = 1 << 31
+
+// ErrHardenedPublic returned if an attempt to use hardened derivation with a public key was made
 var ErrHardenedPublic = errors.New("hdw: can't use hardened derivation with public key")
 
+// PrivateKey is an extended private key bearing the chain code required for derivation of child keys
 type PrivateKey interface {
-	Sign(rand io.Reader, message []byte, opts crypto.SignerOpts) (signature []byte, err error)
-	Public() crypto.PublicKey
+	crypto.Signer
+	// Equal reports whether receiver and x have the same value
 	Equal(x crypto.PrivateKey) bool
+	// Chain returns the chain code
 	Chain() []byte
+	// Derive returns a child key of the receiver using a single index
 	Derive(index uint32) (PrivateKey, error)
+	// Derive returns a child key of the receiver using a full path
 	DerivePath(path Path) (PrivateKey, error)
+	// Public returns the extended public key corresponding to the receiver
 	ExtendedPublic() PublicKey
+	// Naked returns the naked private key that can be used with the standard Go crypto library
 	Naked() crypto.PrivateKey
 }
 
+// PrivateKey is an extended public key bearing the chain code required for derivation of child keys
 type PublicKey interface {
+	// Equal reports whether receiver and x have the same value
 	Equal(x crypto.PublicKey) bool
+	// Chain returns the chain code
 	Chain() []byte
+	// Derive returns a child key of the receiver using single index
 	Derive(index uint32) (PublicKey, error)
+	// Derive returns a child key of the receiver using a full path
 	DerivePath(path Path) (PublicKey, error)
+	// Naked returns the naked public key that can be used with the standard Go crypto library
 	Naked() crypto.PublicKey
 }
 
+// NewSeedFromMnemonic generates the seed value from the mnemonic as specified in BIP-39.
+// The seed is the primary secret used to derive root keys and their derivatives
 func NewSeedFromMnemonic(mnemonic string, password string) []byte {
 	return pbkdf2.Key([]byte(mnemonic), []byte("mnemonic"+password), 2048, 64, sha512.New)
 }
 
+// Path is a BIP-32/SLIP-10 derivation path
 type Path []uint32
 
+// HasPrefix reports whether receiver has a given prefix
 func (p Path) HasPrefix(prefix Path) bool {
 	if len(prefix) > len(p) {
 		return false
@@ -56,6 +79,7 @@ func (p Path) HasPrefix(prefix Path) bool {
 	return true
 }
 
+// String formats the path to the BIP-32/SLIP-10 textual form
 func (path Path) String() string {
 	var out strings.Builder
 	out.WriteByte('m')
@@ -69,6 +93,7 @@ func (path Path) String() string {
 	return out.String()
 }
 
+// ParsePath parses s as a BIP-32/SLIP-10 path, like "m/1'/2"
 func ParsePath(s string) (Path, error) {
 	if len(s) == 0 {
 		return Path{}, nil
@@ -96,6 +121,7 @@ func ParsePath(s string) (Path, error) {
 	return out, nil
 }
 
+// Derive returns a child key of root using a full path. root must be either PrivateKey or PublicKey
 func Derive(root any, path Path) (any, error) {
 	switch k := root.(type) {
 	case PrivateKey:
